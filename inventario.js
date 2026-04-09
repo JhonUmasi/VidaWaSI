d92ade39ea47bf8ae69c61afd3fd91cc342ab1ef
JhonUmasi <jhonumasi13@gmail.com>
2026-04-09 18:36:40 -0500

    Update inventario.js

--- inventario.js
+++ inventario.js
@@ -1,1 +1,46 @@
-
+[
+  {
+    "id": "cp-001",
+    "marca": "C.P.",
+    "categoria": "Blusas",
+    "nombre": "Blusa Floral de Seda",
+    "talla": "S",
+    "estado": "10/10",
+    "precio": 45.00,
+    "foto": "assets/img/productos/cp-001.avif",
+    "disponible": true
+  },
+  {
+    "id": "cp-002",
+    "marca": "C.P.",
+    "categoria": "Vestidos",
+    "nombre": "Vestido Midi Cruzado",
+    "talla": "M",
+    "estado": "9/10",
+    "precio": 85.00,
+    "foto": "assets/img/productos/cp-002.avif",
+    "disponible": true
+  },
+  {
+    "id": "cp-003",
+    "marca": "C.P.",
+    "categoria": "Blusas",
+    "nombre": "Top Básico Acanalado",
+    "talla": "L",
+    "estado": "Nuevecito",
+    "precio": 30.00,
+    "foto": "assets/img/productos/cp-003.avif",
+    "disponible": true
+  },
+  {
+    "id": "cp-004",
+    "marca": "Otras Marcas",
+    "categoria": "Pantalones",
+    "nombre": "Jeans Rectos Tiro Alto",
+    "talla": "30",
+    "estado": "8/10",
+    "precio": 55.00,
+    "foto": "assets/img/productos/cp-004.avif",
+    "disponible": false
+  }
+]
